c3f4218df1f16b6e2fe1ac2be1078d2b2086fdae
Ryanson Jonathan <71144324+pinterbanget@users.noreply.github.com>
2024-10-07 17:07:02 +0100

    add function to calculate P

--- proj2.R
+++ proj2.R
@@ -32,7 +32,6 @@ meanlog <- 3.152; sdlog <- 0.451
 infection_to_death <- dlnorm(1:80, meanlog, sdlog)
 infection_to_death_normalized <- infection_to_death / sum(infection_to_death)
 
-#how to normalize?
 
 sum(infection_to_death)
 
@@ -44,3 +43,27 @@ death_day <- rep(days,deaths)
 
 infection_duration <- sample(1:80,n,prob=infection_to_death_normalized, replace = TRUE)
 t0 <- death_day - infection_duration 
+
+
+# below is function to calculate p
+pearson_eval <- function(actual_deaths, simulated_deaths) {
+  # Evaluates the fitness of the model compared to the actual data.
+  # Parameters:
+  #   actual_deaths (vector): actual data containing actual deaths
+  #   simulated_deaths (vector): simulated data
+  # Returns:
+  #   p (num): the final error calculated by the modified Pearson formula
+  
+  # Sets p to store the sum of values by elements.
+  p <- 0
+  
+  # Loops through all data.
+  for (i in 1:length(actual_deaths)) {
+    di <- actual_deaths[i]
+    di_s <- simulated_deaths[i]
+    p <- p + ((di - di_s)**2 / max(c(1, di_s)))
+  }
+  
+  # Returns the sum of it all
+  return(p)
+}
