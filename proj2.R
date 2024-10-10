545d5fc3d14894757daf6d71ca8da55fa9eb0254
Ryanson Jonathan <71144324+pinterbanget@users.noreply.github.com>
2024-10-10 16:18:38 +0100

    add comments for pearson_eval

--- proj2.R
+++ proj2.R
@@ -17,54 +17,46 @@
 ##################################
 # ***
 # Sets the working directories for the coders.
-# setwd("/Users/rj/Documents/Codes/StatProg/covidsim") # Ryan's path
+setwd("/Users/rj/Documents/Codes/StatProg/covidsim") # Ryan's path
 # setwd("/Users/josephgill/covidsim") # Joseph's path
 # setwd("/Users/fransiskusbudi/uoe/stat_prog/covidsim") # Frans' path
 
 # Data Loading
 data <- read.table("engcov.txt", header = TRUE)
 data <- data[1:150,]
-days <- data$julian
-deaths <- data$nhs
-
-meanlog <- 3.152
-sdlog <- 0.451
-
-infection_to_death <- dlnorm(1:80, meanlog, sdlog)
-infection_to_death_normalized <- infection_to_death / sum(infection_to_death)
-
-n <- sum(deaths) #29422
-death_day <- rep(days,deaths)
-
-
-infection_duration <- sample(1:80,n,prob=infection_to_death_normalized, replace = TRUE)
-t0 <- death_day - infection_duration 
-
-tabby <- tabulate(death_day, nbins = 310) #how many people died on each day
-
 
 pearson_eval <- function(actual_deaths, simulated_deaths) {
   # Evaluates the fitness of the model compared to the actual data.
+  # A modified Pearson formula is used, where the difference
+  # between each element, di and di_s, is squared and divided by
+  # di_s if di_s > 1, or 1 if di_s < 1; where
+  # di indicates a point from the actual data, and di_s indicates a point
+  # from the simulated data. The sum from all the values is returned as p.
+  #
   # Parameters:
-  #   actual_deaths (vector): actual data containing actual deaths
-  #   simulated_deaths (vector): simulated data
+  #   actual_deaths (vec): actual data containing actual deaths
+  #   simulated_deaths (vec): simulated data
+  #
   # Returns:
-  #   p (num): the final error calculated by the modified Pearson formula
+  #   p (float): the final error calculated by the modified Pearson formula
 
-  # Sets p to store the sum of values by elements.
+  # di and di_s are declared as vectors, 
+  # so vectorisation computation can be done.
   di <- actual_deaths
   di_s <- simulated_deaths
-  pearson_score <- sum((di - di_s)**2 / pmax(di_s, 1))
+  
+  # The modified Pearson score, p, is calculated by summing each element
+  # in the vector after the vectorised calculation.
+  p <- sum((di - di_s)**2 / pmax(di_s, 1))
 
-  # Returns the sum of it all
-  return(pearson_score)
+  return(p)
 }
 
 deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL,
                    meanlog = 3.152, sdlog = 0.451) {
   # blablal
   # Params:
-  #     t (vec)         : 
+  #     t (vec)         :
   #     deaths (vec)    :
   #     n.rep (int)     :
   #     bs (bool)       :
@@ -79,12 +71,22 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL,
   # Calculates the number of deaths occurred from the data.
   n <- sum(deaths)
 
-  # Creates an n-spaced vector to store in which day of the year
-  # each patient died. E.g.: 1 means the patient died on January 1,
-  # 32 means the patient died on February 1.
+  # Creates an n-spaced vector, with each element of the vector representing
+  # a patient, to store the day of the year said patient died, e.g.
+  # 1 means the patient died on January 1, while
+  # 32 means the patient died on February 1, etc.
   death_days <- rep(t, deaths)
-
+  
   # Creates a 310-spaced vector to store the total deaths occurred on each day.
+  # For this simulation, a limit is set where the deaths can only occur
+  # between day 1 inclusive and day 310 inclusive of the year.
+  
+  # To create this vector, two options are available:
+  # 1) bs = TRUE (bootstrapped),
+  # meaning that 
+  # 2) bs = FALSE,
+  # meaning that we use raw death data passed in the arguments (t and deaths).
+  
   total_deaths_per_day <- tabulate(death_days, nbins = 310)
 
   # Generates a probability of possible infection-to-death time for COVID-19
@@ -111,25 +113,18 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL,
   to_pick_1 <- c(-8, -4, -2, -1, 1, 2, 4, 8)
   to_pick_2 <- c(-4, -2, -1, 1, 2, 4)
   to_pick_3 <- c(-2, -1, 1, 2)
-  
-  # sim_inf_days <- sample(1:80, n, prob=normalized_inf_to_d, replace = TRUE)
-  # sim_death_days <- t0 + sim_inf_days
-  # sim_death_days <- pmax(sim_death_days, 1) # done to filter out values < 1
-  # sim_death_days <- pmin(sim_death_days, 310) # done to filter out values > 310
-  # total_sim_deaths_per_day <- tabulate(sim_death_days, nbins = 310)
-  # pearson_score <- pearson_eval(total_deaths_per_day, total_sim_deaths_per_day)
 
   for (i in 1:n.rep) {
-    sim_inf_dur <- sample(1:80, n, prob=normalized_inf_to_d, replace = TRUE)
+    sim_inf_dur <- sample(1:80, n, prob = normalized_inf_to_d, replace = TRUE)
     sim_death_days <- t0 + sim_inf_dur
     sim_death_days <- pmax(sim_death_days, 1) # done to filter out values < 1
     sim_death_days <- pmin(sim_death_days, 310) # done to filter out values > 310
     total_sim_deaths_per_day <- tabulate(sim_death_days, nbins = 310)
     pearson_score <- pearson_eval(total_deaths_per_day, total_sim_deaths_per_day)
 
-    if (i < round(n.rep*0.5)) {
+    if (i < round(n.rep * 0.5)) {
       to_sample <- to_pick_1
-    } else if (i < round(n.rep*0.75)) {
+    } else if (i < round(n.rep * 0.75)) {
       to_sample <- to_pick_2
     } else {
       to_sample <- to_pick_3
@@ -166,8 +161,9 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL,
     matplot(1:310, cbind(t0_freq, total_deaths_per_day, total_sim_deaths_per_day))
   }
   
-  return(t0)
+  return(P)
 }
 
 
-deconv(t = data$julian, deaths = data$nhs, n.rep = 100)
+p_hist <- deconv(t = data$julian, deaths = data$nhs, n.rep = 100)
+plot(log(1:100), log(p_hist))
