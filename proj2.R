0a0cddf7b96c9af2911f4cda5b2e46ed85c6f6c6
Fransiskus Budi Kurnia Agung <fransiskusbudi97@gmail.com>
2024-10-17 15:26:33 +0100

    fixed bug and plot

--- proj2.R
+++ proj2.R
@@ -43,8 +43,8 @@
 ##################################
 # Sets the working directories for the coders.
 # setwd("/Users/rj/Documents/Codes/StatProg/covidsim") # Ryan's path
-setwd("/Users/josephgill/covidsim") # Joseph's path
-# setwd("/Users/fransiskusbudi/uoe/stat_prog/covidsim") # Frans' path
+# setwd("/Users/josephgill/covidsim") # Joseph's path
+setwd("/Users/fransiskusbudi/uoe/stat_prog/covidsim") # Frans' path
 
 
 pearson_eval <- function(real_deaths, sim_deaths) {
@@ -62,16 +62,16 @@ pearson_eval <- function(real_deaths, sim_deaths) {
   # Returns:
   #   p (float)         : the Pearson value calculated by the modified
   #                       Pearson formula.
-
+  
   # di and di_s are declared as vectors,
   # so vectorisation computation can be done.
   di <- real_deaths
   di_s <- sim_deaths
   pearson_val_vec <- (di - di_s)**2 / pmax(di_s, 1)
-
+  
   # The sum of each element in the vectors is the Pearson value.
   pearson_val <- sum(pearson_val_vec)
-
+  
   return(pearson_val)
 }
 
@@ -99,28 +99,28 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
   #                       by t0 for each iteration.
   #     t0 (vec)        : a vector of estimations for the days of infections.
   #     total_sdbd (vec): a vector of simulated days of deaths from the model.
-
+  
   # Calculates the total number of deaths occurred from the data.
   n <- sum(deaths)
-
+  
   # Creates an n-spaced vector, with each element of the vector representing
   # a patient, to store the day of the year said patient died.
   death_days <- rep(t, deaths)
-
+  
   # Creates a 310-spaced vector to store the total deaths that occurred on each day.
   # For this simulation, a limit is set where the deaths can only occur
   # between day 1 (inclusive) and day 310 (inclusive) of the year.
   deaths_by_day <- tabulate(death_days, nbins = 310)
-
+  
   # Generates a probability of possible infection-to-death time for COVID-19
   # patients (the number of days between when a COVID-19 patient contracted
   # COVID-19 and their death). Log normal distribution is used here,
   # with mean = 3.152 and sd = 0.451, in line with the ISARIC study.
   inf_to_d_dist <- dlnorm(1:80, meanlog = 3.152, sdlog = 0.451)
-
+  
   # This distribution is then normalised.
   inf_to_d_dist <- inf_to_d_dist / sum(inf_to_d_dist)
-
+  
   # If not provided, generates t0, an n-spaced vector to estimate the
   # day of COVID-19 infection for each patient.
   # Elements in t0 are gained from subtracting the days of patient deaths
@@ -130,15 +130,15 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
     t0 <- death_days - sample(1:80, n, prob = inf_to_d_dist,
                               replace = TRUE)
   }
-
+  
   # Creates a matrix of shape (310, n.rep) to store the estimated t0
   # for every iteration from 1 to n.rep.
   inft <- matrix(data = NA, nrow = 310, ncol = n.rep)
-
+  
   # Creates an n.rep-spaced vector to store the Pearson score
   # after each iteration.
   P <- rep(0, n.rep)
-
+  
   # Defines three steps to pick for changing t0 during the iterations.
   # The idea is when the iterations are 0-50% complete, a wider range
   # of steps (step_options) can be picked. Then, when it is more than
@@ -148,39 +148,41 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
   step_options <- c(-8, -4, -2, -1, 1, 2, 4, 8)
   step_options_50 <- c(-4, -2, -1, 1, 2, 4)
   step_options_75 <- c(-2, -1, 1, 2)
-
+  
   # Loops over n.rep times.
   for (i in 1:n.rep) {
     # The first part of this loop is to generate the variables that will be
     # used to update t0, together with evaluating the initial Pearson score.
-
+    
     # If bootstrapping is enabled, the number of deaths per day is sampled
     # from a Poisson distribution with mean given by the real data.
     # TODO: add this comment 
     # If not, the number of deaths per day used for the model is the real data.
     if (bs) {
-      total_deaths_by_day <- rpois(length(deaths_by_day),
-                                   lambda = deaths_by_day)
+      death_pois <- rpois(length(deaths),
+                                   lambda = deaths)
+      death_days_pois <- rep(t, death_pois)
+      total_deaths_by_day <- tabulate(death_days_pois, nbins = 310)
     } else {
       total_deaths_by_day <- deaths_by_day
     }
-
+    
     # Samples random infection-to-death times for each patient.
     sim_inf_dur <- sample(1:80, n, prob = inf_to_d_dist, replace = TRUE)
-
+    
     # Adds the times to t0 to obtain simulated days of death.
     sim_death_days <- t0 + sim_inf_dur
-
+    
     # Tabulates the result to get the total simulated deaths by day.
     total_sim_deaths_by_day <- tabulate(sim_death_days, nbins = 310)
-
+    
     # Computes the initial Pearson value for this iteration.
     pearson_value <- pearson_eval(total_deaths_by_day, total_sim_deaths_by_day)
-
+    
     # The second part of this loop is to shift elements in t0 by random steps
     # to try to improve the Pearson value. Because the shifting and evaluation
     # is done per-element, another n-time loop will be done inside this loop.
-
+    
     # Defines which step distribution to sample based on the
     # current iteration number.
     if (i <= round(n.rep * 0.5)) {
@@ -190,37 +192,37 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
     } else {
       step_to_sample <- step_options_75
     }
-
+    
     # Creates an n-spaced vector containing samples of steps.
     random_steps <- sample(step_to_sample, n, replace = TRUE)
-
+    
     # Starts the loop to shift and evaluate elements in t0.
     # Each element in t0 is accessed and updated in a random order
     # to avoid biases.
     for (index in sample(1:n)) {
       # Saves the old day of infection value.
       old_infected_day <- t0[index]
-
+      
       # Gets a new day of infection value by adding a step from random_steps,
       # with filtering to make sure it is within the (1, 310) range.
       new_infected_day <- old_infected_day + random_steps[index]
       new_infected_day <- max(new_infected_day, 1)
       new_infected_day <- min(new_infected_day, 310)
-
+      
       # To calculate the Pearson value, the day of death values are needed.
       # Gets the old day of death value, together with the new day of death.
       old_death_day <- sim_death_days[index]
       new_death_day <- new_infected_day + sim_inf_dur[index]
-
+      
       # Copies the total simulated deaths by day tabulation
       # and modifies according to the new day of death values.
       new_total_sdbd <- total_sim_deaths_by_day
       new_total_sdbd[new_death_day] <- new_total_sdbd[new_death_day] + 1
       new_total_sdbd[old_death_day] <- new_total_sdbd[old_death_day] - 1
-
+      
       # Evaluates the Pearson value with the new day of death values.
       new_pv <- pearson_eval(total_deaths_by_day, new_total_sdbd)
-
+      
       # If the Pearson value decreases,
       # updates the new variables made within this loop.
       if (new_pv < pearson_value) {
@@ -234,17 +236,17 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
     # After t0 has converged, t0 is then tabulated,
     # to get the number of new infections by day.
     t0_freq <- tabulate(t0, nbins = 310)
-
+    
     # This iteration's number of new infections by day is stored in inft.
     inft[, i] <- t0_freq
-
+    
     # Puts the final Pearson score for the i-th iteration to P.
     P[i] <- pearson_value
-
+    
     # Generates three line charts for this iteration, with the x axis
     # being the possible days of data (in this case 1:310),
     # and the y axis being the number of people.
-
+    
     # The first line is the estimated data of COVID-19 infection.
     plot(1:310, t0_freq, col = "black", type = "l", lwd = 2,
          xlab = "Day number", ylab = "Num. of people", ylim = c(0, 1800),
@@ -253,25 +255,25 @@ deconv <- function(t, deaths, n.rep = 100, bs = FALSE, t0 = NULL) {
     # The second line is the actual data of COVID-19 deaths.
     lines(1:310, total_deaths_by_day, col = "blue",
           type = "l", lwd = 1, lty = "solid")
-
+    
     # The third line is the estimated data of COVID-19 deaths.
     lines(1:310, total_sim_deaths_by_day, col = "red",
           type = "l", lwd = 1,lty = "dotdash")
-
+    
     # A line is shown to indicate the first day of lockdown (day 84).
     abline(v = 84, col = "red", lwd = 2)
-
+    
     # A legend is put on the top right of the graph.
     legend(x = "topright", inset = 0.05,
            legend = c("Est. new infections", "Est. deaths", "Actual deaths"),
-           col = c("green", "#302a20", "blue"), lty = 1:2, cex = 0.8)
+           col = c("green", "red", "blue"), lty = 1:2, cex = 0.8)
   }
-
+  
   # Saves the simulation of death days from the final iteration.
   # This is necessary to know the fit of the simulation model
   # vs. the real data.
   total_sdbd <- total_sim_deaths_by_day
-
+  
   return(list(P, inft, t0, total_sdbd))
 }
 
@@ -287,6 +289,7 @@ t <- data$julian
 
 # Gets deaths, the number of deaths happening by day, from the data.
 deaths <- data$nhs
+death_days <- rep(t, deaths)
 
 # Calls the deconv function to run the initial simulation for estimated
 # infection days, just by feeding t and deaths to the function.
@@ -314,7 +317,7 @@ max_inft_bs <- apply(inft_bs,1,function(x)  quantile(x,probs = c(.025,.975))["97
 sim_deaths_tabulated <- initial_sim[[4]]
 
 # Tabulated the actual days of death data to compare with the simulated result.
-actual_deaths_tabulated <- tabulate(deaths, nbins=310)
+actual_deaths_tabulated <- tabulate(death_days, nbins=310)
 
 # matplot(
 #   1:310,
@@ -330,18 +333,19 @@ actual_deaths_tabulated <- tabulate(deaths, nbins=310)
 # )
 plot(1:310,inft_t0[,100], col='black', type ="l", lwd=2,xlab = "Day number",
      ylab = "Num. of people", ylim = c(0, 1800))
-lines(1:310,min_inft_bs, col ='gray', type ='l', lwd =1,lty ='dashed', ylim = c(0, 1800))
-lines(1:310,max_inft_bs, col ='gray', type ='l', lwd =1,lty ='dashed', ylim = c(0, 1800))
-lines(1:310,death_day, col ='blue', type ='l', lwd =1,lty ='solid', ylim = c(0, 1800))
-lines(1:310,sim_deaths, col ='red', type ='l', lwd =1,lty ='dotdash', ylim = c(0, 1800))
-abline(v=84, col ='red', lwd=2,ylim = c(0, 1800))
+# lines(1:310,inft_bs[,100], col ='green', type ='l', lwd =1,lty ='solid')
+lines(1:310,min_inft_bs, col ='gray', type ='l', lwd =1,lty ='dashed')
+lines(1:310,max_inft_bs, col ='gray', type ='l', lwd =1,lty ='dashed')
+lines(1:310,actual_deaths_tabulated, col ='blue', type ='l', lwd =1,lty ='solid')
+lines(1:310,sim_deaths_tabulated, col ='red', type ='l', lwd =1,lty ='dotdash')
+abline(v=84, col ='red', lwd=2)
 
 legend(x = "topright", inset = 0.05,
-       legend = c("Est. new infections", "bs1","bs2" ,"Est. deaths", "Actual deaths","Start of Lockdown"),
-       col = c("green","gray","gray", "red", "blue","red"),lty=c("solid","dashed","dashed","dotdash","solid","solid"), cex=0.8)
+       legend = c("Est. new infections","Est. deaths", "Actual deaths","Start of Lockdown", "Gray Shade 95% Uncertainty"),
+       col = c("black", "red", "blue","red",'gray'),lty=c("solid","dotdash","solid","solid","dotted"), cex=0.8)
 
 x <- 1:length(min_inft_bs)
-polygon(c(x, rev(x)), c(min_inft_bs, rev(max_inft_bs)), col = 'gray', density = 50, border = NA, ylim = c(0, 1800))
+polygon(c(x, rev(x)), c(min_inft_bs, rev(max_inft_bs)), col = 'gray', density = 50, border = NA)
 
 
 # polygon(c(min_inft_bs),c(min_inft_bs),col='gray')
